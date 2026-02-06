d46c8c4a16dd7b1335ae77bc7c8b092fa65fd0bb
Reagan-WebDev <oluochreagano@gmail.com>
2026-02-06 14:04:59 +0300

    Finished My ToDoListApp

--- ToDoList.js
+++ ToDoList.js
@@ -1,6 +1,12 @@
 const taskinput = document.getElementById('taskinput');
 const tasklist = document.getElementById('tasklist');
 
+taskinput.addEventListener("keyup", function (event) {
+    if (event.key === "Enter") {
+        addTask();
+    }
+});
+
 function addTask() {
     if (taskinput.value.trim() === '') {
         alert('Please Enter Task')
@@ -9,8 +15,31 @@ function addTask() {
     }
     else {
         let li = document.createElement('li');
-        li.innerHTNL = taskinput.value;
+        li.innerHTML = taskinput.value;
         tasklist.appendChild(li);
-        taskinput.value = '';
+        let span = document.createElement('span');
+        span.innerHTML = "\u00d7";
+        li.appendChild(span);
     }
+    taskinput.value = "";
+    saveData();
 }
+tasklist.addEventListener("click", function(e){
+    if(e.target.tagName === "LI"){
+        e.target.classList.toggle("checked");
+        saveData();
+    }
+    else if(e.target.tagName === "SPAN"){
+        e.target.parentElement.remove();
+        saveData();
+    }
+}, false)
+
+function saveData() {
+    localStorage.setItem("data", tasklist.innerHTML);
+}
+
+function showTask() {
+    tasklist.innerHTML = localStorage.getItem("data");
+}
+showTask();
